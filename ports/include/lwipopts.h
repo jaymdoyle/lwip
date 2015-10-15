c51b47b9779df6a4139f1e532fe38f7ffcec6a00
Jay Doyle <jay.doyle@vecna.com>
2015-10-15 15:42:12 -0400

    defined DEFAULT_TCP_RECVMBOX_SIZE

--- ports/include/lwipopts.h
+++ ports/include/lwipopts.h
@@ -181,5 +181,6 @@
 #define TCPIP_THREAD_STACKSIZE          8192
 #define TCPIP_MBOX_SIZE                 10
 #define SYS_LWIP_MBOX_SIZE              1500
+#define DEFAULT_TCP_RECVMBOX_SIZE       (sizeof(void *))
 
 #endif /* __LWIPOPTS_H__ */
